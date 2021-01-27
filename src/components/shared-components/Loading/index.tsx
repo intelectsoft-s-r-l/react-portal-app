f40035e4b029c92a9f0de2af5b9c8fdf7466bf78
dipzera <nathanielthomaz@gmail.com>
2021-01-27 14:21:40 +0200

    cover=content is now default prop of Loading component

--- src/components/shared-components/Loading/index.tsx
+++ src/components/shared-components/Loading/index.tsx
@@ -1,32 +1,21 @@
 import React from "react";
 import { Spin } from "antd";
-import PropTypes from "prop-types";
 import { LoadingOutlined } from "@ant-design/icons";
 
 export interface ILoading {
-    align?: string;
-    cover?: string;
+  align?: string;
+  cover?: string;
 }
 
 const Icon = <LoadingOutlined style={{ fontSize: 35 }} spin />;
 
 const Loading = (props: ILoading) => {
-    const { align = "center", cover = "inline" } = props;
-    return (
-        <div className={`loading text-${align} cover-${cover}`}>
-            <Spin indicator={Icon} />
-        </div>
-    );
+  const { align = "center", cover = "content" } = props;
+  return (
+    <div className={`loading text-${align} cover-${cover}`}>
+      <Spin indicator={Icon} />
+    </div>
+  );
 };
-
-// Loading.propTypes = {
-//     size: PropTypes.string,
-//     cover: PropTypes.string,
-// };
-
-// Loading.defaultProps = {
-//     align: "center",
-//     cover: "inline",
-// };
 
 export default Loading;
