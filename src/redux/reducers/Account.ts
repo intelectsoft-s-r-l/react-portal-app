e1b28f49c32104dddd8cbc38409f62d62f42a644
dipzera <nathanielthomaz@gmail.com>
2021-01-27 14:22:30 +0200

    Fixed types of setProfileInfo redux action

--- src/redux/reducers/Account.ts
+++ src/redux/reducers/Account.ts
@@ -2,37 +2,37 @@ import { CLEAR_INFO, UPDATE_SETTINGS } from "../constants/Account";
 import { REMOVE_AVATAR } from "../constants/Account";
 
 export interface IAccount {
-    CompanyID: number;
-    Email?: string;
-    FirstName?: string;
-    ID?: number;
-    LastName?: string;
-    Password?: string;
-    PhoneNumber?: string;
-    Photo?: string;
-    UiLanguage?: number;
+  CompanyID?: number;
+  Email?: string;
+  FirstName?: string;
+  ID?: number;
+  LastName?: string;
+  Password?: string;
+  PhoneNumber?: string;
+  Photo?: string;
+  UiLanguage?: number;
 }
 const initialState = {
-    CompanyID: null,
-    Email: "",
-    FirstName: null,
-    ID: null,
-    LastName: null,
-    Password: null,
-    PhoneNumber: null,
-    Photo: null,
-    UiLanguage: 0,
+  CompanyID: null,
+  Email: "",
+  FirstName: null,
+  ID: null,
+  LastName: null,
+  Password: null,
+  PhoneNumber: null,
+  Photo: null,
+  UiLanguage: 0,
 };
 const account = (state = initialState, action: any) => {
-    switch (action.type) {
-        case UPDATE_SETTINGS:
-            return {
-                ...state,
-                ...action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case UPDATE_SETTINGS:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default account;
