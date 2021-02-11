b0707a094006613c81aa35ec3b16a60aef66f81a
dipzera <nathanielthomaz@gmail.com>
2021-02-11 14:35:37 +0200

    added mail service functions

---
+++ src/api/mail/index.ts
@@ -0,0 +1,25 @@
+import { API_MAIL_URL } from "../../configs/AppConfig";
+import HttpService from "../HttpService";
+import { ApiDecorator, ApiResponse } from "../types";
+import { IMail, ITemplate } from "./types";
+
+type TokenResponse = ApiDecorator<ApiResponse, "Token", string>;
+export class MailService extends HttpService {
+  public constructor() {
+    super(API_MAIL_URL);
+  }
+
+  public SendMail = async (data: IMail) =>
+    this.instance.post<TokenResponse>("/SendMail", data);
+
+  public UpdateTemplate = async (data: ITemplate) =>
+    this.instance.post<TokenResponse>("/UpdateTemplate", data);
+
+  public GetTemplates = async (APIKey: string) =>
+    this.instance.get<ApiDecorator<ApiResponse, "Templates", ITemplate[]>>(
+      "/GetTemlates",
+      {
+        params: { APIKey },
+      }
+    );
+}
