02fef1c463e0745382f57d62a00585b2d79f3e5b
dipzera <nathanielthomaz@gmail.com>
2021-02-17 14:42:53 +0200

    backToTop component was added

--- src/layouts/app-layout/index.tsx
+++ src/layouts/app-layout/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Grid } from "antd";
+import { Layout, Grid, BackTop } from "antd";
 import { connect } from "react-redux";
 import SideNav from "../../components/layout-components/SideNav";
 import TopNav from "../../components/layout-components/TopNav";
@@ -10,10 +10,10 @@ import Footer from "../../components/layout-components/Footer";
 import AppViews from "../../views/app-views";
 import navigationConfig from "../../configs/NavigationConfig";
 import {
-    SIDE_NAV_WIDTH,
-    SIDE_NAV_COLLAPSED_WIDTH,
-    NAV_TYPE_SIDE,
-    NAV_TYPE_TOP,
+  SIDE_NAV_WIDTH,
+  SIDE_NAV_COLLAPSED_WIDTH,
+  NAV_TYPE_SIDE,
+  NAV_TYPE_TOP,
 } from "../../constants/ThemeConstant";
 import Utils from "../../utils";
 import { ITheme } from "../../redux/reducers/Theme";
@@ -23,59 +23,54 @@ const { Content } = Layout;
 const { useBreakpoint } = Grid;
 
 export const AppLayout = ({ navCollapsed, navType, location }: any) => {
-    const currentRouteInfo = Utils.getRouteInfo(
-        navigationConfig,
-        location.pathname
-    );
-    const screens = Utils.getBreakPoint(useBreakpoint());
-    const isMobile = !screens.includes("lg");
-    const isNavSide = navType === NAV_TYPE_SIDE;
-    const isNavTop = navType === NAV_TYPE_TOP;
+  const currentRouteInfo = Utils.getRouteInfo(
+    navigationConfig,
+    location.pathname
+  );
+  const screens = Utils.getBreakPoint(useBreakpoint());
+  const isMobile = !screens.includes("lg");
+  const isNavSide = navType === NAV_TYPE_SIDE;
+  const isNavTop = navType === NAV_TYPE_TOP;
 
-    const getLayoutGutter = () => {
-        if (isNavTop || isMobile) {
-            return 0;
-        }
-        return navCollapsed ? SIDE_NAV_COLLAPSED_WIDTH : SIDE_NAV_WIDTH;
-    };
-    return (
-        <Layout>
-            <HeaderNav isMobile={isMobile} />
-            {isNavTop && !isMobile ? (
-                <TopNav routeInfo={currentRouteInfo} />
-            ) : null}
-            <Layout className="app-container">
-                {isNavSide && !isMobile ? (
-                    <SideNav routeInfo={currentRouteInfo} />
-                ) : null}
-                <Layout
-                    className="app-layout"
-                    style={{ paddingLeft: getLayoutGutter() }}
-                >
-                    <div
-                        className={`app-content ${
-                            isNavTop ? "layout-top-nav" : ""
-                        }`}
-                    >
-                        <PageHeader
-                            display={currentRouteInfo?.breadcrumb}
-                            title={currentRouteInfo?.title}
-                        />
-                        <Content>
-                            <AppViews />
-                        </Content>
-                    </div>
-                    <Footer />
-                </Layout>
-            </Layout>
-            {isMobile && <MobileNav />}
+  const getLayoutGutter = () => {
+    if (isNavTop || isMobile) {
+      return 0;
+    }
+    return navCollapsed ? SIDE_NAV_COLLAPSED_WIDTH : SIDE_NAV_WIDTH;
+  };
+  return (
+    <Layout>
+      <HeaderNav isMobile={isMobile} />
+      {isNavTop && !isMobile ? <TopNav routeInfo={currentRouteInfo} /> : null}
+      <Layout className="app-container">
+        {isNavSide && !isMobile ? (
+          <SideNav routeInfo={currentRouteInfo} />
+        ) : null}
+        <Layout
+          className="app-layout"
+          style={{ paddingLeft: getLayoutGutter() }}
+        >
+          <div className={`app-content ${isNavTop ? "layout-top-nav" : ""}`}>
+            <PageHeader
+              display={currentRouteInfo?.breadcrumb}
+              title={currentRouteInfo?.title}
+            />
+            <Content>
+              <AppViews />
+            </Content>
+          </div>
+          <BackTop />
+          <Footer />
         </Layout>
-    );
+      </Layout>
+      {isMobile && <MobileNav />}
+    </Layout>
+  );
 };
 
 const mapStateToProps = ({ theme }: IState) => {
-    const { navCollapsed, navType, locale } = theme as ITheme;
-    return { navCollapsed, navType, locale };
+  const { navCollapsed, navType, locale } = theme as ITheme;
+  return { navCollapsed, navType, locale };
 };
 
 export default connect(mapStateToProps)(React.memo(AppLayout));
