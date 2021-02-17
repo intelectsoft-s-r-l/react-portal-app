5f206f305d1af0f6958803e7c864fc556a4440a9
dipzera <nathanielthomaz@gmail.com>
2021-02-17 09:41:12 +0200

    react quill migration

--- src/views/app-views/applications/single-app-page/Mail/templates/CreateTemplateModal.tsx
+++ src/views/app-views/applications/single-app-page/Mail/templates/CreateTemplateModal.tsx
@@ -1,6 +1,9 @@
-import { Form, Input, Modal } from "antd";
 import React, { ChangeEvent, useState } from "react";
+import ReactQuill from "react-quill";
+import { Form, Input, message, Modal } from "antd";
 import TextEditor from "../../TextEditor";
+import { MailService } from "../../../../../../api/mail";
+import { EnErrorCode } from "../../../../../../api/HttpService";
 
 interface ICreateTemplateModal {
   APIKey: string;
@@ -16,26 +19,30 @@ const CreateTemplateModal = ({
 }: ICreateTemplateModal) => {
   const [loading, setLoading] = useState(false);
   const [form] = Form.useForm();
-  const [textEditor, setTextEditor] = useState({
-    Name: "",
-    Body: "",
-    Subject: "",
-  });
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setTextEditor((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    console.log(values);
+    return await new MailService()
+      .UpdateTemplate({
+        Name: values.Name,
+        Subject: Buffer.from(JSON.stringify(values.Subject)).toString("base64"),
+        Body: Buffer.from(JSON.stringify(values.Body)).toString("base64"),
+        APIKey,
+      })
+      .then((data) => {
+        setLoading(false);
+        if (data && data.ErrorCode === EnErrorCode.NO_ERROR) {
+          message.success("Template created!");
+        }
+      });
   };
-  const onEditorChange = (text: string, name: string) => {
-    setTextEditor((prev) => ({
-      ...prev,
-      [name]: text,
-    }));
-  };
+
   const onOk = (values: any) => {
-    console.log(textEditor);
+    form.validateFields().then(async (values) => {
+      await onFinish(values);
+      close();
+    });
   };
   return (
     <Modal
@@ -47,20 +54,13 @@ const CreateTemplateModal = ({
     >
       <Form name="createTemplateForm" layout="vertical" form={form}>
         <Form.Item name="Name" label="Name">
-          <Input name="Name" onChange={onChange} />
+          <Input />
         </Form.Item>
-        <Form.Item name="Subject" label="Subject">
-          <TextEditor
-            handleEditorChange={(text: string) =>
-              onEditorChange(text, "Subject")
-            }
-          />
+        <Form.Item name="Subject" label="Subject" initialValue="">
+          <ReactQuill />
         </Form.Item>
-        <Form.Item label="Body">
-          <TextEditor
-            name="Body"
-            handleEditorChange={(text: string) => onEditorChange(text, "Body")}
-          />
+        <Form.Item label="Body" initialValue="" name="Body">
+          <ReactQuill />
         </Form.Item>
       </Form>
     </Modal>
