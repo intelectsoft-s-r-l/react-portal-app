411227f801d71c3ffd6a52b7b8fc50b5feeceafd
dipzera <nathanielthomaz@gmail.com>
2021-03-23 17:55:14 +0200

    Added virtual phone to visualize sms message !Not ready

---
+++ src/components/util-components/Phone/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Card } from "antd";
+import "./phone.scss";
+
+const Phone = (props: any) => {
+  return (
+    <Card>
+      <div className="phone"></div>
+    </Card>
+  );
+};
+export default Phone;
