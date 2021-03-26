751e0bcf2770c6f36a6d64a577883d2526ceaa37
dipzera <nathanielthomaz@gmail.com>
2021-03-26 16:57:09 +0200

    Added possibility to download and scan qr code

--- src/views/auth-views/other/fiscal/index.tsx
+++ src/views/auth-views/other/fiscal/index.tsx
@@ -1,38 +1,36 @@
-import { Button, Card, Divider, Result, Typography } from "antd";
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { createRef, useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Tooltip,
+  Card,
+  Divider,
+  Modal,
+  Result,
+  Typography,
+} from "antd";
+import { exportComponentAsPNG } from "react-component-export-image";
+import { ShareAltOutlined, DownloadOutlined } from "@ant-design/icons";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { EnErrorCode } from "../../../../api";
 import { BillService } from "../../../../api/bill-service";
-import { IBill, IBillItems } from "../../../../api/bill-service/types";
+import { IBill } from "../../../../api/bill-service/types";
 import ErrorHandlePage from "../../../../components/shared-components/ErrorHandlePage";
 import Flex from "../../../../components/shared-components/Flex";
 import Loading from "../../../../components/shared-components/Loading";
-import {
-  APP_PREFIX_PATH,
-  AUTH_PREFIX_PATH,
-} from "../../../../configs/AppConfig";
-import { SIGNOUT } from "../../../../redux/constants/Auth";
+import { APP_PREFIX_PATH, SUBDIR_PATH } from "../../../../configs/AppConfig";
 import Utils from "../../../../utils";
 import { useQuery } from "../../../../utils/hooks/useQuery";
+const QRCode = require("qrcode.react");
 const { Text } = Typography;
-function getVatPercent(arr: IBillItems[], EnVat: "A" | "B" | "C") {
-  return arr
-    .filter((item) => item.VATCode === EnVat)
-    .map((item) => item.VATValue.toFixed(2));
-}
-const Fiscal = () => {
+const Fiscal = ({ location }: RouteComponentProps) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [hasAccess, setHasAccess] = useState<boolean>(true);
   const instance = new BillService();
   const [billInfo, setBillInfo] = useState<IBill | undefined>(undefined);
   const [billCompany, setBillCompany] = useState<string>("");
-  const [vatCodeTotal, setVatCodeTotal] = useState<any>({
-    a: 0,
-    b: 0,
-    c: 0,
-  });
+  const [isShare, setIsShare] = useState(false);
   const query = useQuery();
+  const ref = createRef<any>();
   const getBillInfo = () => {
     return instance
       .GetBillInfo(query.get("bill")!, query.get("device")!)
@@ -46,19 +44,16 @@ const Fiscal = () => {
           }));
           setBillCompany(data.Company);
 
-          // The above is some awful code that needs refactoring (:
-          let a = data.Bill?.BillItems.filter(
-            (item) => item.VATCode === "A"
-          ).reduce((acc, curr) => acc + curr.VATTotal, 0);
-
-          let b = data.Bill?.BillItems.filter(
-            (item) => item.VATCode === "B"
-          ).reduce((acc, curr) => acc + curr.VATTotal, 0);
-
-          let c = data.Bill?.BillItems.filter(
-            (item) => item.VATCode === "C"
-          ).reduce((acc, curr) => acc + curr.VATTotal, 0);
-          setVatCodeTotal({ a, b, c });
+          const vatArray = data.Bill.BillItems.reduce((acc, current) => {
+            // @ts-ignore
+            if (!acc.some((x: any) => x.VATCode === current.VATCode)) {
+              // @ts-ignore
+              acc.push(current);
+            }
+            return acc;
+          }, []).sort((a: any, b: any) =>
+            a.VATCode < b.VATCode ? -1 : a.VATCode < b.VATCode ? 1 : 0
+          );
         }
       });
   };
@@ -86,121 +81,190 @@ const Fiscal = () => {
     );
   return (
     <div
-      className="h-100 d-flex justify-content-center align-items-center"
+      className="d-flex justify-content-center align-items-center"
       style={{
         backgroundImage: `url(${process.env.PUBLIC_URL}/img/others/img-17.jpg)`,
         backgroundSize: "cover",
+        minHeight: "100%",
       }}
     >
-      <Card style={{ width: 350, maxWidth: "100%" }}>
-        <Flex alignItems="center" flexDirection="column">
-          <Text>"{billCompany}"</Text>
-          <Text>IDNO: {billInfo.IDNO}</Text>
-          <Text className="text-center">{billInfo.Address}</Text>
-          <Text>
-            Inr. Nr:{" "}
-            {billInfo.FiscalNumber.length === 0 ? "--" : billInfo.FiscalNumber}{" "}
-          </Text>
-          <span>{billInfo.FreeTextHeader}</span>
-        </Flex>
-        <Flex justifyContent="between">
-          <div>
-            <Text>{billInfo!.ShiftNumber ?? ""}</Text>
-            <br />
-            <Text>#-{billInfo.Workplace}</Text>
-            <br />
-            <Text>#-{billInfo.User}</Text>
-          </div>
-        </Flex>
-
-        {billInfo.BillItems &&
-          billInfo.BillItems.map((item, idx) => (
-            <Flex justifyContent="between" className="mt-3" key={idx}>
-              {/* We map over BillItems here */}
-              <div>
-                <div>&nbsp;</div>
-                <Text>{item.Name}</Text>
-              </div>
-              <div className="text-right">
-                <Text style={{ letterSpacing: 2 }}>
-                  {item.Quantity.toFixed(2)} Litri x {item.BasePrice}
-                </Text>
-                <br />
-                <Text>{item.Summ.toFixed(2)}</Text>
-              </div>
-            </Flex>
-          ))}
-        <Divider dashed />
-        <Flex justifyContent="between">
-          <div>
-            <Text className="h4 font-weight-bold">TOTAL</Text>
-            <br />
-            {billInfo.Discount > 0 && <Text>Reducere:</Text>}
-          </div>
-          <div className="text-right">
-            <Text className="h3 font-weight-bold">
-              {(billInfo!.Summ - billInfo!.Discount).toFixed(2)}
-            </Text>
-            <br />
-            {billInfo.Discount > 0 && (
-              <Text>{billInfo!.Discount.toFixed(2)}</Text>
-            )}
-          </div>
-        </Flex>
-        <Flex justifyContent="between" className="my-3">
-          <div>
-            {vatCodeTotal.a > 0 && (
-              <div>TVA A={getVatPercent(billInfo.BillItems, "A") + "%"}</div>
-            )}
-            {vatCodeTotal.b > 0 && (
-              <div>TVA B={getVatPercent(billInfo.BillItems, "B") + "%"}</div>
-            )}
-            {vatCodeTotal.c > 0 && (
-              <div>TVA C={getVatPercent(billInfo.BillItems, "C") + "%"}</div>
-            )}
-            <div>{billInfo.PaymantType}</div>
-          </div>
-          <div className="text-right">
-            {vatCodeTotal.a > 0 && <div>{vatCodeTotal.a.toFixed(2)}</div>}
-            {vatCodeTotal.b > 0 && <div>{vatCodeTotal.b.toFixed(2)}</div>}
-            {vatCodeTotal.c > 0 && <div>{vatCodeTotal.c.toFixed(2)}</div>}
-            <div>{(billInfo!.Summ - billInfo!.Discount).toFixed(2)}</div>
-          </div>
-        </Flex>
-        <Text>#Card: &nbsp;&nbsp;{billInfo.Client}</Text>
-        <Flex justifyContent="center" className="my-3 text-center">
-          <div>
-            <Text style={{ textTransform: "uppercase", letterSpacing: 10 }}>
-              Va multumim!
-            </Text>
-            <br />
-            <div>
-              <Text>
-                {billInfo.TotalArticle}{" "}
-                {billInfo.TotalArticle > 1 ? "ARTICOLE" : "ARTICOL"}
-              </Text>
-            </div>
-          </div>
-        </Flex>
-        <Flex justifyContent="between">
-          <div>
-            <Text>{Utils.padNumber(billInfo.Number)}</Text>
-          </div>
-          <div className="text-right">
-            <Text>{billInfo.Date.split(" ")[0]}</Text>
-            <br />
-            <Text>{billInfo.Date.split(" ")[1]}</Text>
-          </div>
-        </Flex>
-        <Flex
-          alignItems="center"
-          flexDirection="column"
-          className="text-center"
+      <div ref={ref} style={{ margin: "15px 0" }}>
+        <Card
+          style={{
+            width: 350,
+            maxWidth: "100%",
+            margin: "0",
+          }}
         >
-          <Text className="text-uppercase h3">Bon fiscal!</Text>
-          <div>{billInfo.FreeTextFooter}</div>
-        </Flex>
-      </Card>
+          <Flex alignItems="center" flexDirection="column">
+            <Text>"{billCompany}"</Text>
+            <Text>IDNO: {billInfo.IDNO}</Text>
+            <Text className="text-center">{billInfo.Address}</Text>
+            <Text>
+              Inr. Nr:{" "}
+              {billInfo.FiscalNumber.length === 0
+                ? "--"
+                : billInfo.FiscalNumber}{" "}
+            </Text>
+            <span>{billInfo.FreeTextHeader}</span>
+          </Flex>
+          <Flex justifyContent="between" className="mb-3">
+            <div>
+              <Text>{Utils.padNumber(billInfo!.ShiftNumber) ?? ""}</Text>
+              <br />
+              <Text>#-{billInfo.Workplace}</Text>
+              <br />
+              <Text>#-{billInfo.User}</Text>
+            </div>
+          </Flex>
+
+          {billInfo.BillItems &&
+            billInfo.BillItems.map((item, idx) => (
+              <div>
+                {/* We map over BillItems here */}
+                <div>
+                  <Text>
+                    {item.Name.length > 31
+                      ? item.Name.substr(0, 31)
+                      : item.Name}
+                  </Text>
+                </div>
+                <div className="text-right">
+                  <Text style={{ letterSpacing: 1 }}>
+                    {item.Quantity.toFixed(2)} x {item.BasePrice.toFixed(2)} =
+                    {item.Summ.toFixed(2)}
+                  </Text>
+                </div>
+              </div>
+            ))}
+          <Divider dashed className="mb-2 mt-3" />
+          <div>
+            <Flex justifyContent="between">
+              <Text className="h4 font-weight-bold m-0">TOTAL</Text>
+              <Text className="h4 font-weight-bold m-0">
+                {(billInfo!.Summ - billInfo!.Discount).toFixed(2)}
+              </Text>
+            </Flex>
+            {billInfo.Discount > 0 && (
+              <Flex justifyContent="between">
+                <Text>Reducere:</Text>
+                <Text>{billInfo!.Discount.toFixed(2)}</Text>
+              </Flex>
+            )}
+          </div>
+          <Flex flexDirection="column">
+            {billInfo.BillItems.reduce((acc, current) => {
+              // @ts-ignore
+              if (!acc.some((x: any) => x.VATCode === current.VATCode)) {
+                // @ts-ignore
+                acc.push(current);
+              }
+              return acc;
+            }, [])
+              .sort((a: any, b: any) =>
+                a.VATCode < b.VATCode ? -1 : a.VATCode < b.VATCode ? 1 : 0
+              )
+              .map((el: any, index: number, arr: any) => {
+                const totalVat = billInfo.BillItems.filter(
+                  (bil) => bil.VATCode === el.VATCode
+                  // @ts-ignore
+                ).reduce((acc, curr) => acc + curr.VATTotal, 0);
+
+                return (
+                  <Flex justifyContent="between">
+                    <div>
+                      TVA {el.VATCode}={el.VATValue.toFixed(2)}%
+                    </div>
+                    <div>{totalVat.toFixed(2)}</div>
+                  </Flex>
+                );
+              })}
+          </Flex>
+          <Flex justifyContent="between">
+            <div>{billInfo.PaymantType}</div>
+            <div>{billInfo.Summ.toFixed(2)}</div>
+          </Flex>
+          {billInfo.Change > 0 && (
+            <Flex justifyContent="between">
+              <div>REST</div>
+              <div>{billInfo.Change.toFixed(2)}</div>
+            </Flex>
+          )}
+          <Text>{billInfo.Client && `#Card: ${billInfo.Client}`}</Text>
+          <Text></Text>
+          <Flex justifyContent="center" className="my-3 text-center">
+            <div>
+              <Text style={{ textTransform: "uppercase", letterSpacing: 10 }}>
+                Va multumim!
+              </Text>
+              <br />
+              <div>
+                <Text style={{ letterSpacing: 1 }}>
+                  {billInfo.TotalArticle}{" "}
+                  {billInfo.TotalArticle > 1 ? "ARTICOLE" : "ARTICOL"}
+                </Text>
+              </div>
+            </div>
+          </Flex>
+          <Flex justifyContent="between">
+            <div>
+              <Text>{Utils.padNumber(billInfo.Number)}</Text>
+            </div>
+            <div className="text-right">
+              <Text style={{ letterSpacing: 1 }}>{billInfo.Date}</Text>
+            </div>
+          </Flex>
+          <Flex
+            alignItems="center"
+            flexDirection="column"
+            className="text-center"
+          >
+            <Text className="text-uppercase h1">Bon fiscal!</Text>
+            <QRCode
+              value={`https://eservicii.md${SUBDIR_PATH}/auth/fiscal?bill=${query.get(
+                "bill"
+              )}&device=${query.get("device")}`}
+            />
+            <small className="mt-2">
+              Scaneaza codul pentru vizualizarea bonului online!
+            </small>
+            <div>{billInfo.FreeTextFooter}</div>
+          </Flex>
+        </Card>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "fixed",
+          right: 30,
+          bottom: 30,
+        }}
+      >
+        <Tooltip title="Descarca .png" className="mb-2" placement="left">
+          <Button shape="circle" onClick={() => exportComponentAsPNG(ref)}>
+            <DownloadOutlined />
+          </Button>
+        </Tooltip>
+        {navigator.share && (
+          <Tooltip title="Share" placement="left">
+            <Button
+              shape="circle"
+              type="primary"
+              onClick={() => {
+                navigator.share({
+                  title: "Bon fiscal",
+                  url: `https://eservicii.md${SUBDIR_PATH}/auth/fiscal?bill=${query.get(
+                    "bill"
+                  )}&device=${query.get("device")}`,
+                });
+              }}
+            >
+              <ShareAltOutlined />
+            </Button>
+          </Tooltip>
+        )}
+      </div>
     </div>
   );
 };
